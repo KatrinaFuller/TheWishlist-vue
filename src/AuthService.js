bf17a28739f827487b6908127ad7ed4f6ef38d0a
Katrina Fuller <trinafuller@att.net>
2019-11-10 15:12:37 -0700

    able to log in and out

--- src/AuthService.js
+++ src/AuthService.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
-// let baseUrl = location.host.includes('localhost') ? 'https://localhost:5001/' : '/'
-let baseUrl = location.host.includes('localhost')
+let baseUrl = location.host.includes('localhost') ? 'https://localhost:5001/' : '/'
+// let baseUrl = location.host.includes('localhost')
 
 let auth = Axios.create({
   baseURL: baseUrl + "account/",
---
+++ src/views/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container-fluid bg">
+    <div class="row">
+      <div class="col">
+        <h1 class="my-5">WishList</h1>
+      </div>
+    </div>
+    <div class="login my-3">
+      <form v-if="loginForm" @submit.prevent="loginUser">
+        <input class="m-1" type="email" v-model="creds.email" placeholder="email" />
+        <input class="m-1" type="password" v-model="creds.password" placeholder="password" />
+        <button class="btn m-2" type="submit">Login</button>
+      </form>
+      <form v-else @submit.prevent="register">
+        <input class="m-1" type="text" v-model="newUser.username" placeholder="name" />
+        <input class="m-1" type="email" v-model="newUser.email" placeholder="email" />
+        <input class="m-1" type="password" v-model="newUser.password" placeholder="password" />
+        <button class="btn m-2" type="submit">Create Account</button>
+      </form>
+      <div @click="loginForm = !loginForm">
+        <p v-if="loginForm">No account Click to Register</p>
+        <p v-else>Already have an account click to Login</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      loginForm: true,
+      creds: {
+        email: "",
+        password: ""
+      },
+      newUser: {
+        email: "",
+        password: "",
+        username: ""
+      }
+    };
+  },
+  beforeCreate() {
+    if (this.$store.state.user.id) {
+      this.$router.push({ name: "home" });
+    }
+  },
+  methods: {
+    register() {
+      this.$store.dispatch("register", this.newUser);
+    },
+    loginUser() {
+      this.$store.dispatch("login", this.creds);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.bg {
+  min-height: 100vh;
+}
+</style>
